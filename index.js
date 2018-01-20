84b8b7aa9fcc23b450d76477bcdc8b372964ff7f
CMunkyDev <matt@jcmueller.com>
2018-01-20 12:30:02 -0800

    get package outputting zip object

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+const JSZip = require('jszip')
 const fs = require('fs')
-const backendObject = require('./exampleObject')
 
 let packageTemplate = require('./folder_gens/root/package_template')
 let serverTemplate = require('./folder_gens/root/server_template')
@@ -7,27 +7,28 @@ let indexFileTemplate = require('./folder_gens/root/index_template')
 let routeFileTemplate = require('./folder_gens/routes/route_template')
 let controllerClassFile = require('./folder_gens/controllers/controller_superclass')
 let controllerFileTemplate = require('./folder_gens/controllers/controller_template')
-let modelClassTemplate = require('./folder_gens/models/file_model_superclass')
+let modelClassFile = require('./folder_gens/models/file_model_superclass')
 let modelFileTemplate = require('./folder_gens/models/file_model_template')
-fs.mkdirSync('./test_server_af')
-fs.writeFileSync('./test_server_af/package.json', packageTemplate(backendObject))
-fs.writeFileSync('./test_server_af/server.js', serverTemplate(backendObject.resources))
-fs.mkdirSync('./test_server_af/routes')
-fs.writeFileSync('./test_server_af/routes/index.js', indexFileTemplate(backendObject.resources, 'Router'))
-backendObject.resources.forEach(resource => fs.writeFileSync(`./test_server_af/routes/${resource.name}.js`, routeFileTemplate(resource)))
-fs.mkdirSync('./test_server_af/controllers')
-fs.writeFileSync('./test_server_af/controllers/index.js', indexFileTemplate(backendObject.resources, 'Controller'))
-fs.writeFileSync('./test_server_af/controllers/controller.js', controllerClassFile)
-backendObject.resources.forEach(resource => fs.writeFileSync(`./test_server_af/controllers/${resource.name}.js`, controllerFileTemplate(resource)))
-fs.mkdirSync('./test_server_af/models')
-fs.writeFileSync('./test_server_af/models/index.js', indexFileTemplate(backendObject.resources, 'Model'))
-fs.writeFileSync('./test_server_af/models/model.js', modelClassTemplate)
-fs.mkdirSync('./test_server_af/database')
-backendObject.resources.forEach(resource => fs.writeFileSync(`./test_server_af/database/${resource.name}.json`, JSON.stringify([])))
-backendObject.resources.forEach(resource => fs.writeFileSync(`./test_server_af/models/${resource.name}.js`, modelFileTemplate(resource)))
-function createBE (bEObject) {
-    
+
+function multipleFiles(zip, folderName, extension, resourceArr, inputOrCallback) {
+    resourceArr.forEach(resource => zip.file(`${folderName}/${resource.name}.${extension}`, typeof inputOrCallback === 'string' ? inputOrCallback : inputOrCallback(resource)))
 }
 
-module.exports = createBE
+function createBE(bEObject) {
+    let zip = new JSZip
+    let root = bEObject.name
+    zip.folder(root).file(`package.json`, packageTemplate(bEObject)).file(`server.js`, serverTemplate(bEObject.resources))
+    zip.file(`${root}/routes/index.js`, indexFileTemplate(bEObject.resources, 'Router'))
+    multipleFiles(zip, `${root}/routes`, 'js', bEObject.resources, routeFileTemplate)
+    zip.file(`${root}/controllers/index.js`, indexFileTemplate(bEObject.resources, 'Controller'))
+    zip.file(`${root}/controllers/controller.js`, controllerClassFile)
+    multipleFiles(zip, `${root}/controllers`, 'js', bEObject.resources, controllerFileTemplate)
+    zip.file(`${root}/models/index.js`, indexFileTemplate(bEObject.resources, 'Controller'))
+    zip.file(`${root}/models/model.js`, modelClassFile)
+    multipleFiles(zip, `${root}/models`, 'js', bEObject.resources, modelFileTemplate)
+    multipleFiles(zip, `${root}/database`, 'json', bEObject.resources, '[]')
+    return zip
+}
 
+
+module.exports = createBE
