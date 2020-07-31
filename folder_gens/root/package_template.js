597911516373b1e58cea3699a7471861cab4df9d
CMunkyDev <matt@jcmueller.com>
2020-07-31 14:25:37 -0700

    remove extra space in templates and update jszip dependency version

--- folder_gens/root/package_template.js
+++ folder_gens/root/package_template.js
@@ -1,24 +1,23 @@
 function generatePackageTemplate (backendObject) {
-    return `
-            {
-                "name": "${backendObject.name.toLowerCase()}",
-                "version": "1.0.0",
-                "description": "",
-                "main": "index.js",
-                "scripts": {
-                    "start": "node server.js"
-                },
-                "author": "frankenserver",
-                "license": "ISC",
-                "dependencies": {
-                    "body-parser": "^1.18.2",
-                    "cors": "^2.8.4",
-                    "dotenv": "^4.0.0",
-                    "express": "^4.16.2",
-                    "morgan": "^1.9.0",
-                    "uuid": "^3.2.1"
-                }
-            }
-        `
+    return `{
+    "name": "${backendObject.name.toLowerCase()}",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+        "start": "node server.js"
+    },
+    "author": "frankenserver",
+    "license": "ISC",
+    "dependencies": {
+        "body-parser": "^1.18.2",
+        "cors": "^2.8.4",
+        "dotenv": "^4.0.0",
+        "express": "^4.16.2",
+        "morgan": "^1.9.0",
+        "uuid": "^3.2.1"
+    }
+}
+`
 }
 module.exports = generatePackageTemplate
